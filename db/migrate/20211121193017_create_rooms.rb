8bff2d0ea31236db97d68e57b6c85e28054a8e39
houzairkoussa <houzairmk@icloud.com>
2021-11-21 23:31:33 +0400

    migrated db with rooms

--- db/migrate/20211121193017_create_rooms.rb
+++ db/migrate/20211121193017_create_rooms.rb
@@ -2,7 +2,8 @@ class CreateRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :rooms do |t|
       t.string :name
-
+      t.string :unique_name
+      t.string :room_sid
       t.timestamps
     end
   end

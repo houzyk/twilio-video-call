c5195ac8e59f58b696c9ad3288e85a9107996074
houzairkoussa <houzairmk@icloud.com>
2021-11-21 23:47:15 +0400

    config twillo api

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TwilioVideoCall
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    dotenv::Railtie.load
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

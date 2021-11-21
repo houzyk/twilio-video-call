5cf2b34e1de02a70a7ad3456368e9171ebec9e43
houzairkoussa <houzairmk@icloud.com>
2021-11-22 00:04:05 +0400

    completed video chat app

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,6 +8,24 @@ class RoomsController < ApplicationController
 
   # GET /rooms/1 or /rooms/1.json
   def show
+    @client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
+    unless @room.room_sid.present?
+      # create room in twilio
+      twilio_room = @client.video.rooms.create(type: 'peer-to-peer',unique_name: @room.unique_name)
+      @room.update(room_sid: twilio_room.sid)
+    end
+    identity = (0...5).map { ('a'..'z').to_a[rand(26)] }.join
+    @room_name = @room.unique_name
+
+    #create token to access Twilio room
+    @token = Twilio::JWT::AccessToken.new(ENV['ACCOUNT_SID'], ENV['API_KEY_SID'],ENV['API_KEY_SECRET'], identity: identity)
+
+    #create video grant for token
+    grant = Twilio::JWT::AccessToken::VideoGrant.new
+    grant.room = @room_name
+
+    @token.add_grant grant
+    @token = @token.to_jwt
   end
 
   # GET /rooms/new
--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,13 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
 
 module TwilioVideoCall
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    dotenv::Railtie.load
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
